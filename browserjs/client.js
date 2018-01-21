a47989290670c10dc132cb43e3c6806ef5af76d8
stawrocek <stasiukoza1234@gmail.com>
2018-01-21 17:49:51 +0100

    refactoring

--- browserjs/client.js
+++ browserjs/client.js
@@ -1,17 +1,33 @@
+const loadedImages = {};
+let canvas;
+let ctx;
+let socket;
+
+function prepareImages(imagesLoadedCB){
+    let images = ['kenobi.png'];
+    let promiseArray = images.map(function(imgurl){
+    let prom = new Promise(function(resolve,reject){
+        let img = new Image();
+        img.onload = function(){
+            loadedImages[imgurl] = img;
+            resolve();
+        };
+        img.src = imgurl;
+    });
+    return prom;
+    });
+    Promise.all(promiseArray).then(imagesLoadedCB);
+}
 
 function main(){
-    //var myGamePiece = new component(789, 487, "kenobi.png", 10, 120, "image");
-    // var kenobi = new Image();
-    // kenobi.src = "kenobi.gif";
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    //ctx.font = "30px Comic Sans MS";
-    //ctx.fillStyle = "red";
-    //ctx.textAlign = "center";
-    //ctx.fillText("Hello there", canvas.width / 2, canvas.height / 2); 
-    var kenobi = document.getElementById("kenobi");
-    ctx.drawImage(kenobi, 10, 10);
-    var socket = io();
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext("2d");
+
+    prepareImages(onResourcesLoaded);
+}
+
+function onResourcesLoaded(){
+    socket = io();
     socket.on('connect_error', function (m) { log("connect_error "); });
     socket.on('connect', function (m) { 
         console.log("socket.io connection open"); 
@@ -20,4 +36,7 @@ function main(){
     socket.on('message', function(data) {
         console.log(data);
     });
+
+
+    ctx.drawImage(loadedImages['kenobi.png'], 10, 10);
 }
