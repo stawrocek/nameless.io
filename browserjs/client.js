9642a8ca1579c0ee1709cbf4027eba7708fe0824
stawrocek <stasiukoza1234@gmail.com>
2018-01-21 17:05:43 +0100

    working socket.io

--- browserjs/client.js
+++ browserjs/client.js
@@ -1,3 +1,8 @@
 function main(){
-    alert('xD');
+    var socket = io();
+    socket.on('connect_error', function (m) { log("connect_error "); });
+    socket.on('connect', function (m) { console.log("socket.io connection open"); });
+    socket.on('message', function(data) {
+        console.log(data);
+    });
 }
