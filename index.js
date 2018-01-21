613bf5e6169c55b83ac431da73911bb17b519167
stawrocek <stasiukoza1234@gmail.com>
2018-01-21 14:55:28 +0100

    int main(int argc, char* argv[])

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const io = require('socket.io');
+const io = require('socket.io')(http);;
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.engine("html", ejs.renderFile);
-app.set('views', './views');
+app.set('views', './templates');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -16,10 +16,17 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(express.static(__dirname + '/static/'));
+app.use(express.static(__dirname + '/browserjs/'));
 
 app.get('/', (req, res) => {
     var model = {};
     res.render('index.html', model);
+});
+
+io.on('connection', s => {
+  console.log('socket.io connection');
+  for (var t = 0; t < 3; t++)
+    setTimeout(() => s.emit('message', 'message from server'), 1000*t);
 });
 
 app.use((req,res,next) => {
