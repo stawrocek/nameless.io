2705a9db232c659be27b5b36b86af73c1f25f06f
LEGOracer <mpostawka@gmail.com>
2018-01-21 17:31:39 +0100

    kebobi

--- browserjs/client.js
+++ browserjs/client.js
@@ -1,3 +1,14 @@
+
 function main(){
-    alert('xD');
+    //var myGamePiece = new component(789, 487, "kenobi.png", 10, 120, "image");
+    // var kenobi = new Image();
+    // kenobi.src = "kenobi.gif";
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    //ctx.font = "30px Comic Sans MS";
+    //ctx.fillStyle = "red";
+    //ctx.textAlign = "center";
+    //ctx.fillText("Hello there", canvas.width / 2, canvas.height / 2); 
+    var kenobi = document.getElementById("kenobi");
+    ctx.drawImage(kenobi, 10, 10);
 }
